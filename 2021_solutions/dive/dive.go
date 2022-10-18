708c74743bb9fa84c606e423dd59b86b2194d4e8
Cerys Pinch <cerys.pinch@hpe.com>
2022-10-18 13:12:34 +0100

    Add soltuion to puzzle 2 of dive (day 2)

--- 2021_solutions/dive/dive.go
+++ 2021_solutions/dive/dive.go
@@ -39,9 +39,9 @@ func readInput() ([]string, error) {
 	return inputList, nil
 }
 
-// get_Horizontal_Position_And_Depth calculates the final horizontal
+// get_Horizontal_Position_And_Depth_One calculates the final horizontal
 // position and depth of the submarine using the input list
-func get_Horizontal_Position_And_Depth() (int, int, error) {
+func get_Horizontal_Position_And_Depth_One() (int, int, error) {
 	inputList, err := readInput()
 	if err != nil {
 		fmt.Println("error")
@@ -75,15 +75,76 @@ func get_Horizontal_Position_And_Depth() (int, int, error) {
 	return horizontalPosition, depth, nil
 }
 
+// get_Horizontal_Position_And_Depth_Two calculates the final horizontal
+// position and depth of the submarine using the input list
+func get_Horizontal_Position_And_Depth_Two() (int, int, error) {
+	inputList, err := readInput()
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	horizontalPosition := 0
+	depth := 0
+	direction := ""
+	value := 0
+	i := 0
+	aim := 0
+
+	for _, input := range inputList {
+		// Split input line into array with two items (direction and value)
+		sections := strings.Fields(input)
+		direction = sections[i]
+		valueAsString := sections[i+1]
+		value, _ = strconv.Atoi(valueAsString)
+
+		if direction == "down" {
+			aim = aim + value
+		} else if direction == "up" {
+			aim = aim - value
+		} else {
+			horizontalPosition = horizontalPosition + value
+			depth = depth + (aim * value)
+		}
+
+		// Reset sections for next line of input
+		sections = nil
+	}
+
+	return horizontalPosition, depth, nil
+}
+
 // Puzzle_One returns the answer to puzzle one of dive (day 2 of AoC),
 // it multiplies the horizontal position and depth to return the product
-func Puzzle_One() int {
-	horizontalPosition, depth, _ := get_Horizontal_Position_And_Depth()
+func Puzzle_One() (int, error) {
+	horizontalPosition, depth, err := get_Horizontal_Position_And_Depth_One()
+	if err != nil {
+		return 0, err
+	}
 	product := horizontalPosition * depth
-	return product
+	return product, nil
+}
+
+// Puzzle_Two returns the answer to puzzle one of dive (day 2 of AoC),
+// it multiplies the horizontal position and depth to return the product
+func Puzzle_Two() (int, error) {
+	horizontalPosition, depth, err := get_Horizontal_Position_And_Depth_Two()
+	if err != nil {
+		return 0, err
+	}
+	product := horizontalPosition * depth
+	return product, nil
 }
 
 func main() {
-	result := Puzzle_One()
-	fmt.Println(result)
+	result1, err := Puzzle_One()
+	if err != nil {
+		fmt.Println("There was an error getting the solution to puzzle 1")
+	}
+	fmt.Println(result1)
+
+	result2, err := Puzzle_Two()
+	if err != nil {
+		fmt.Println("There was an error getting the solution to puzzle 2")
+	}
+	fmt.Println(result2)
 }
